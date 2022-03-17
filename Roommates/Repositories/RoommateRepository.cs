9b692fa2ab18f55e95d3bc5a4db64271326f4ff2
cjgochanour <cjgochanour@gmail.com>
2022-03-17 13:59:41 -0500

    added getall method to roommaterepository

--- Roommates/Repositories/RoommateRepository.cs
+++ Roommates/Repositories/RoommateRepository.cs
@@ -1,11 +1,42 @@
 ﻿using Microsoft.Data.SqlClient;
 using Roommates.Models;
+using System.Collections.Generic;
 
 namespace Roommates.Repositories
 {
     internal class RoommateRepository : BaseRepository
     {
         public RoommateRepository(string connectionString) : base(connectionString) { }
+        public List<Roommate> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, FirstName FROM Roommate";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Roommate> roommates = new List<Roommate>();
+
+                        while (reader.Read())
+                        {
+                            int idValue = reader.GetInt32(reader.GetOrdinal("Id"));
+                            string firstNameValue = reader.GetString(reader.GetOrdinal("Name"));
+                            Roommate mate = new Roommate
+                            {
+                                Id = idValue,
+                                FirstName = firstNameValue,
+                            };
+                            roommates.Add(mate);
+                        }
+                        return roommates;
+                    }
+                }
+            }
+        }
         public Roommate GetById(int id)
         {
             using (SqlConnection conn = Connection)
