8a3ded588c2ab5d76a18371bb25cd0727463e3de
cjgochanour <cjgochanour@gmail.com>
2022-03-17 11:03:37 -0500

    created RoommateRepository

---
+++ Roommates/Repositories/RoommateRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+
+namespace Roommates.Repositories
+{
+    internal class RoommateRepository : BaseRepository
+    {
+        public RoommateRepository(string connectionString) : base(connectionString) { }
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Roommate.FirstName, Roommate.RentPortion, Room.Name
+                                        FROM Roommate
+                                        JOIN Room ON Room.Id = Roommate.RoomId
+                                        WHERE Roommate.Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Roommate mate = null;
+
+                        if (reader.Read())
+                        {
+                            mate = new Roommate()
+                            {
+                                Id = id,
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion")),
+                                Room = new Room()
+                                {
+                                    Name = reader.GetString(reader.GetOrdinal("Name"))
+                                }
+                            };
+                        }
+                        return mate;
+                    }
+                }
+            }
+        }
+    }
+}
